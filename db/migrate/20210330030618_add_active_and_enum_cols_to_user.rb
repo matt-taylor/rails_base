35c737474d3d75a7288f32f4b0b434ba65ff612d
Matt Taylor <mattius.taylor@gmail.com>
2021-04-01 03:04:33 -0700

    by the time I am satisfied with admins...it deserves a bigass jump in minor version since I cant do major yet

--- db/migrate/20210330030618_add_active_and_enum_cols_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddActiveAndEnumColsToUser < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :admin, :integer, default: 0
-    add_column :users, :active, :boolean, default: true
-  end
-end
