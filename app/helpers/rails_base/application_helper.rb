c3973c698009dab94e3dd2c7293cd47c18f197da
Matt Taylor <mattius.taylor@gmail.com>
2021-04-21 19:16:36 -0700

    WOAAHHHH DARK MODE!!!!; library to keep track of cookie client side

--- app/helpers/rails_base/application_helper.rb
+++ app/helpers/rails_base/application_helper.rb
@@ -1,4 +1,18 @@
 module RailsBase::ApplicationHelper
+  APPEARANCE_MODE_COOKIE = "_#{Rails.application.class.parent_name}_appearance_mode".gsub(' ', '-').downcase
+  APPEARANCE_TEXT_CLASS = 'color-text-class'
+
+  def appearance_mode_drop_down
+    current = cookies[APPEARANCE_MODE_COOKIE]
+    current = cookies[APPEARANCE_MODE_COOKIE]
+    types = RailsBase::Configuration::Appearance::APPEARANCE_TYPES
+    logger.fatal { "Failed to find the cookie. Given[#{current&.to_sym}]. expected #{types}" }
+    unless types.include?(current&.to_sym)
+      cookies[APPEARANCE_MODE_COOKIE] = current = RailsBase.appearance.default_mode
+    end
+    { types: types, current: current }
+  end
+
   def browser
     @browser ||= Browser.new(request.user_agent)
   end
