a5f6a38a328912e4e9ee430f9aba3ffdfbf5124a
Matt Taylor <mattius.taylor@gmail.com>
2021-04-24 00:53:52 -0700

    comments

--- lib/rails_base/engine.rb
+++ lib/rails_base/engine.rb
@@ -12,10 +12,10 @@ module RailsBase
 
     initializer 'define magic convenionce methods for converting team', after: 'active_record.initialize_database' do |app|
       # Only execute when not doing DB actions
-      boolean = defined?(ARGV) ? true : false
-      boolean = false if boolean && ARGV[0]&.include?('db')
-      boolean = false if boolean && ARGV[0]&.include?('asset')
-      boolean = false if boolean && ARGV[0]&.include?(':')
+      boolean = defined?(ARGV) ? true : false  # for when no ARGVs are provided, we know its a railsc or rails s explicit
+      boolean = false if boolean && ARGV[0]&.include?('db') # when its the DB rake tasks
+      boolean = false if boolean && ARGV[0]&.include?('asset') # when its an asset
+      boolean = false if boolean && ARGV[0]&.include?(':') # else this delim should never be included
       if boolean
         puts ARGV
         # need to eager load Models
