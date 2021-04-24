e4c2811f60b9c6d7929778459241bf94a9c6f830
Matt Taylor <mattius.taylor@gmail.com>
2021-04-23 17:53:53 -0700

    gut using the jquery cookie, too much hassle; clean up what mode is used

--- app/assets/javascripts/rails_base/application.js
+++ app/assets/javascripts/rails_base/application.js
@@ -13,10 +13,9 @@
 // = require rails-ujs
 // = require activestorage
 // = require turbolinks
-// =require jquery3
 // = require_tree .
-
-// =require popper
-// =require bootstrap-sprockets
+// = require jquery3
+// = require popper
+// = require bootstrap-sprockets
 // = require allow_numeric
 // = require jquery.mask
