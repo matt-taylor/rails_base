0a71aad02603793476e664af7b1f5d030e40e940
Matt Taylor <mattius.taylor@gmail.com>
2021-04-28 22:02:50 -0700

    viewport stuff

--- app/helpers/rails_base/appearance_helper.rb
+++ app/helpers/rails_base/appearance_helper.rb
@@ -3,6 +3,22 @@ module RailsBase::AppearanceHelper
   APPEARANCE_MODE_ACTUAL_COOKIE = "_#{Rails.application.class.parent_name}_appearance_actual_mode".gsub(' ', '-').downcase
   APPEARANCE_TEXT_CLASS = RailsBase::Configuration::Display::Text::APPEARANCE_TEXT_CLASS
 
+  VIEWPORT_EXTRA_SMALL = 'xs'.freeze
+  VIEWPORT_SMALL = 'sm'.freeze
+  VIEWPORT_MEDIUM = 'md'.freeze
+  VIEWPORT_LARGE = 'lg'.freeze
+  VIEWPORT_EXTRA_LARGE = 'xl'.freeze
+
+  VIEWPORT_MOBILE_MAX = VIEWPORT_MEDIUM
+
+  VIEWPORT_SIZES = {
+    VIEWPORT_EXTRA_SMALL => 576,
+    VIEWPORT_SMALL => 768,
+    VIEWPORT_MEDIUM => 992,
+    VIEWPORT_LARGE => 1200,
+    VIEWPORT_EXTRA_LARGE => nil,
+  }
+
   def appearance_text_class
     APPEARANCE_TEXT_CLASS
   end
