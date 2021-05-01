1dd2fe0fbd556bb01ddcea2ff2cf43c1f545a9d0
Matt Taylor <mattius.taylor@gmail.com>
2021-05-01 10:48:42 -0700

    move stuff around

--- lib/rails_base/engine.rb
+++ lib/rails_base/engine.rb
@@ -3,22 +3,16 @@ module RailsBase
     isolate_namespace RailsBase
 
     initializer 'instantiate RailsBase configs' do |_app|
-      RailsBase.config
+      RailsBase.config if ___execute_initializer___?
     end
 
     initializer 'remove write access to RailsBase config', after: 'after_initialize' do |app|
-      RailsBase::Configuration::Base._unset_allow_write!
+      RailsBase::Configuration::Base._unset_allow_write! if ___execute_initializer___?
     end
 
     initializer 'define magic convenionce methods for converting team', after: 'active_record.initialize_database' do |app|
-      # Only execute when not doing DB actions
-      boolean = defined?(ARGV) ? true : false  # for when no ARGVs are provided, we know its a railsc or rails s explicit
-      boolean = false if boolean && ARGV[0]&.include?('db') # when its the DB rake tasks
-      boolean = false if boolean && ARGV[0]&.include?('asset') # when its an asset
-      boolean = false if boolean && ARGV[0]&.include?(':') # else this delim should never be included
-      boolean = false if boolean && ENV['SKIP_CUSTOM_INIT']=='true' # explicitly set the variable to skip shit
-
-      if boolean
+      if ___execute_initializer___?
+        raise
         # need to eager load Models
         Rails.application.eager_load!
 
@@ -43,6 +37,15 @@ module RailsBase
           app.config.paths["db/migrate"] << expanded_path
         end
       end
+    end
+
+    def ___execute_initializer___?
+      # Only execute when not doing DB actions
+      boolean = defined?(ARGV) ? true : false  # for when no ARGVs are provided, we know its a railsc or rails s explicit
+      boolean = false if boolean && ARGV[0]&.include?('db') # when its the DB rake tasks
+      boolean = false if boolean && ARGV[0]&.include?('asset') # when its an asset
+      boolean = false if boolean && ARGV[0]&.include?(':') # else this delim should never be included
+      boolean = false if ENV['SKIP_CUSTOM_INIT']=='true' # explicitly set the variable to skip shit
     end
   end
 end
