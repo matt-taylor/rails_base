dfe254bc3a0272b0ace34ba191054bd384c0eff7
Matt Taylor <mattius.taylor@gmail.com>
2021-04-30 22:48:53 -0700

    fix link tree for asset building downstream

--- app/assets/config/rails_base/manifest.js
+++ app/assets/config/rails_base/manifest.js
@@ -1,3 +1,3 @@
-//= link_tree ../../images/rails_base
+//= link_tree ../../images/rails_base/
 //= link_directory ../../javascripts/rails_base .js
 //= link_directory ../../stylesheets/rails_base .css
