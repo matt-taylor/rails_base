2400c6763e186db5b3842323324ddc18b34d1679
Matt Taylor <mtaylor@chime.com>
2021-06-07 00:02:37 -0700

    conflict

--- lib/sidekiq/web_custom/version.rb
+++ lib/sidekiq/web_custom/version.rb
@@ -4,7 +4,7 @@ module Sidekiq
   module WebCustom
     MAJOR = 0  # With backwards incompatability. Requires annoucment and update documentation
     MINOR = 1  # With feature launch. Documentation of upgrade is useful via a changelog
-    PATCH = 0  # With minor upgrades or patcing a small bug. No changelog necessary
+    PATCH = 1  # With minor upgrades or patcing a small bug. No changelog necessary
     VERSION = [MAJOR,MINOR,PATCH].join('.')
 
     def self.get_version
