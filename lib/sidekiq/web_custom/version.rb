72af02a8710c932459051088db9da39c41fd5d21
Matt Taylor <mtaylor@chime.com>
2021-06-06 23:57:42 -0700

    test

--- lib/sidekiq/web_custom/version.rb
+++ lib/sidekiq/web_custom/version.rb
@@ -6,5 +6,9 @@ module Sidekiq
     MINOR = 1  # With feature launch. Documentation of upgrade is useful via a changelog
     PATCH = 0  # With minor upgrades or patcing a small bug. No changelog necessary
     VERSION = [MAJOR,MINOR,PATCH].join('.')
+
+    def self.get_version
+      puts VERSION
+    end
   end
 end
