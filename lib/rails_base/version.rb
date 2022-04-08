06c70cd8421638e55663e510c8fcff87364dd022
Matt Taylor <mattius.taylor@gmail.com>
2022-04-08 10:02:39 -0700

    test out publish

--- lib/rails_base/version.rb
+++ lib/rails_base/version.rb
@@ -2,7 +2,7 @@ module RailsBase
   MAJOR = '0'
   MINOR = '70'
   PATCH = '1'
-  VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}"
+  VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}.pre"
 
   def self.print_version
     $stdout.puts VERSION
