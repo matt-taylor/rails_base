aae146a4d6649404ab5b624f146473951675eaca
Matt Taylor <mattius.taylor@gmail.com>
2024-08-20 20:55:11 -0700

    ruby 3.2 reconfigure mail

--- app/mailers/rails_base/event_mailer.rb
+++ app/mailers/rails_base/event_mailer.rb
@@ -1,16 +1,18 @@
 class RailsBase::EventMailer < RailsBase::ApplicationMailer
   default from: Rails.configuration.mail_from
 
-  def send_sso(user:, message:)
+  def send_sso(user, message)
     @user = user
     @message = message
     mail(to: user.email, subject: "#{RailsBase.app_name}: SSO login", template_name: 'event')
     # event(user: user, event: 'SSO login', message: message)
   end
 
-  def event(user:, event:, message:)
+  def event(user, event, message)
     @user = user
     @message = message
     mail(to: @user.email, subject: "#{RailsBase.app_name}: #{event}", template_name: 'event')
   end
+
+  include ::RailsBase::MailerKwargInject
 end
