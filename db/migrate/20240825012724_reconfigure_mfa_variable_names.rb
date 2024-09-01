a0ef53364798ffadd42071098af9c8bbb470b747
Matt Taylor <mattius.taylor@gmail.com>
2024-09-01 14:35:30 -0700

    forgot to delete rename -- fixed in original migration

--- db/migrate/20240825012724_reconfigure_mfa_variable_names.rb
+++ db/migrate/20240825012724_reconfigure_mfa_variable_names.rb
@@ -1,10 +1,5 @@
 class ReconfigureMfaVariableNames < ActiveRecord::Migration[6.1]
   def change
-    ###
-    # DELETE THIS FROM THIS MIGRATION -- This gets added to the migration before
-    rename_column :users, :otp_required_for_login, :mfa_otp_enabled
-    ####
-
     ## Rename original MFA columns to SMS specific columns
     rename_column :users, :mfa_enabled, :mfa_sms_enabled
     rename_column :users, :last_mfa_login, :last_mfa_sms_login
